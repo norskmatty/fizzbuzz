4f77825df30d506232b5aad97788fea4094f79af
norskmatty <matthewconley00@gmail.com>
2016-05-26 11:08:09 -0700

    fizzbuzz2

--- js/js.js
+++ js/js.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-	for(var i=1;i<=100;i++) {
+	$('form').submit(function() {
+		var value = parseInt($("#number").val());
+		if(isNaN(value)) {
+			alert('Please enter an integer between 1 and 1000');
+			$('#number').val('');
+			return false;
+		}
+		else if(value > 1000) {
+			alert('Please enter an integer between 1 and 1000');
+			$('#number').val('');
+			return false;
+		}
+		else {
+			$('#number').val('');
+			fizzbuzz(value);
+			return false;
+		}
+	});
+
+	
+});
+
+function fizzbuzz(number) {
+	for(var i=1;i<=number;i++) {
 		if(i%3 == 0 && i%5 == 0) {
-			$('body').append('FizzBuzz' + '<br>');
+					$('body').append('FizzBuzz' + '<br>');
 		}
 		else if(i%3 == 0) {
 			$('body').append('Fizz' + '<br>');
@@ -10,7 +33,7 @@ $(document).ready(function() {
 			$('body').append('Buzz' + '<br>');
 		}
 		else {
-		$('body').append(i + '<br>');
+			$('body').append(i + '<br>');
 		}
 	}
-})
+} 
